e10baeced756364b7c5e8c46fd08abaa5207f2a0
Divyansh Srivastava <divyanshsrivastava9125@gmail.com>
2019-10-23 00:25:01 +0530

    A slight change in the html view.

--- Music/views.py
+++ Music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -13,3 +13,18 @@ def index(request):
 def details(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'Music/detail.html', {'album': album})
+
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExists):
+        return  render(request, 'Music/detail.html', {
+            'album': album,
+            'error_message': "You did not select any song"
+        })
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'Music/detail.html', {'album': album})
